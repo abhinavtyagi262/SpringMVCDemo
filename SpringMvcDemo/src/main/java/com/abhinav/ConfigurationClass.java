4ac919cdc53e47478829c6babf9d5184ced52822
abhinavtyagi262 <61839001+abhinavtyagi262@users.noreply.github.com>
2020-03-05 21:22:59 +0530

    Initial commit.

---
+++ SpringMvcDemo/src/main/java/com/abhinav/ConfigurationClass.java
@@ -0,0 +1,13 @@
+package com.abhinav;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.abhinav")
+public class ConfigurationClass extends WebMvcConfigurerAdapter {
+
+}
